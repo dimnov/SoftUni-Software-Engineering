5a9e5a6498575d17405cfa69db6c25756b806a02
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-16 23:57:10 +0300

    Update 03Next-Day.js

--- JavaScript-Fundamentals/More-Exercises/01Basic-Syntax-Conditional-Statements-And-Loops-More-Exercise/03Next-Day.js
+++ JavaScript-Fundamentals/More-Exercises/01Basic-Syntax-Conditional-Statements-And-Loops-More-Exercise/03Next-Day.js
@@ -1,6 +1,6 @@
 function calcNextDay(year, month, day) {
   var date = new Date(year, month - 1, day);
-  var oneDay = 24 * 60 * 60 * 1000;//86 400 000 milliseconds in one day
+  var oneDay = 24 * 60 * 60 * 1000;
   var nextDate = new Date(date.getTime() + oneDay);
   console.log(nextDate.getFullYear() + "-" + (nextDate.getMonth() + 1) + "-" + nextDate.getDate());
 }

e2a7143efb2e3bcbc74568ee20d432348eca0115
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-17 00:33:19 +0300

    Update 01Sort-Numbers.js

--- JavaScript-Fundamentals/More-Exercises/01Basic-Syntax-Conditional-Statements-And-Loops-More-Exercise/01Sort-Numbers.js
+++ JavaScript-Fundamentals/More-Exercises/01Basic-Syntax-Conditional-Statements-And-Loops-More-Exercise/01Sort-Numbers.js
@@ -1,40 +1,7 @@
-function sortNumbers(input) {
-  let num1 = input[0];
-  let num2 = input[1];
-  let num3 = input[2];
-
-  if (num1 >= num2 && num1 >= num3) {
-    console.log(num1);
-    if (num2 >= num3) {
-      console.log(num2);
-      console.log(num3);
-    }
-    else {
-      console.log(num3);
-      console.log(num2);
-    }
-  }
-  if (num2 >= num3 && num2 >= num1) {
-    console.log(num2);
-    if (num1 > num3) {
-      console.log(num1);
-      console.log(num3);
-    }
-    else {
-      console.log(num3);
-      console.log(num1);
-    }
-  }
-  if (num3 >= num2 && num3 >= num1) {
-    console.log(num3);
-    if (num1 >= num2) {
-      console.log(num1);
-      console.log(num2);
-    }
-    else {
-      console.log(num2);
-      console.log(num1);
-    }
-  }
+function sortNumbers(firstNum, secondNum, thirdNum) {
+  let arrayNumbers = [];
+  arrayNumbers.push(firstNum, secondNum, thirdNum);
+  let sorted = arrayNumbers.sort((a, b) => b - a);
+  console.log(sorted.join('\n'));
 }
-sortNumbers([420, 1337, 69])
+sortNumbers(420, 1337, 69)

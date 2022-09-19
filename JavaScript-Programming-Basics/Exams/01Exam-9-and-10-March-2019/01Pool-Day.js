3135491347119d3473bbba6d5bfa06345cb72570
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-19 23:20:03 +0300

    Create 01Pool-Day.js

---
+++ JavaScript-Programming-Basics/Exams/01Exam-9-and-10-March-2019/01Pool-Day.js
@@ -0,0 +1,10 @@
+function poolDay(input) {
+  let people = Number(input[0]);
+  let fee = Number(input[1]);
+  let loungerPrice = Number(input[2]);
+  let umbrellaPrice = Number(input[3]);
+
+  let
+
+}
+poolDay()

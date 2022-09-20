cf612d233140db850fe4fcfc095c0d4f4e7d9516
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-20 15:47:27 +0300

    Update 01Pool-Day.js

--- JavaScript-Programming-Basics/Exams/01Exam-9-and-10-March-2019/01Pool-Day.js
+++ JavaScript-Programming-Basics/Exams/01Exam-9-and-10-March-2019/01Pool-Day.js
@@ -4,7 +4,13 @@ function poolDay(input) {
   let loungerPrice = Number(input[2]);
   let umbrellaPrice = Number(input[3]);
 
-  let
+  let totalFee = people * fee;
+  let totalLounger = Math.ceil(people * 0.75);
+  let totalLounderPrice = totalLounger * loungerPrice;
+  let totalUmbrella = Math.ceil(people / 2);
+  let totalUmbrellaPrice = totalUmbrella * umbrellaPrice;
 
+  let total = totalFee + totalLounderPrice + totalUmbrellaPrice;
+  console.log(`${total.toFixed(2)} lv.`);
 }
-poolDay()
+poolDay([50, 6, 8, 4])

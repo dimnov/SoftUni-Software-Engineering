94fd326a7e43f3a1019e5be4168bc0dab50b101e
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-28 23:52:29 +0300

    Create 04Reverse-In-Place.js

---
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Lab/04Reverse-In-Place.js
@@ -0,0 +1,9 @@
+function reverseInPlace(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let tempElement = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = tempElement;
+  }
+  console.log(arr.join(' '));
+}
+reverseInPlace(['a', 'b', 'c', 'd', 'e'])

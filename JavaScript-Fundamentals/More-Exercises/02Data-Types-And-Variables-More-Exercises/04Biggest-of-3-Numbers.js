c7a5c95375144e594470eacc0722ca7f63e7de37
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-29 13:12:09 +0300

    Create 04Biggest-of-3-Numbers.js

---
+++ JavaScript-Fundamentals/More-Exercises/02Data-Types-And-Variables-More-Exercises/04Biggest-of-3-Numbers.js
@@ -0,0 +1,16 @@
+function biggestNum(num1, num2, num3) {
+  if (num1 >= num2) {
+    if (num1 >= num3) {
+      console.log(num1);
+    } else if (num2 >= num3) {
+      console.log(num2);
+    } else {
+      console.log(num3);
+    }
+  } else if (num2 >= num3) {
+    console.log(num2);
+  } else {
+    console.log(num3);
+  }
+}
+biggestNum(1, 2, 3)

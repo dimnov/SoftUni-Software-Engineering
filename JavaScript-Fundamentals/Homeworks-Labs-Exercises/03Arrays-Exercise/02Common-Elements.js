fa67b28065804aeb6cd528c7ea048061597160b9
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-30 23:38:22 +0300

    Create 02Common-Elements.js

---
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Exercise/02Common-Elements.js
@@ -0,0 +1,8 @@
+function commonElements(arrOne, arrTwo) {
+  for (const element of arrOne) {
+    if (arrTwo.includes(element)) {
+      console.log(element);
+    }
+  }
+}
+commonElements(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2'])

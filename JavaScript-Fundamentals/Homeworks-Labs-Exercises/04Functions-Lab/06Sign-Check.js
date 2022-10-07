4174829f475ea3de304178edb5beeeb5d7285b40
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-08 01:03:41 +0300

    Update 06Sign-Check.js

--- JavaScript-Fundamentals/Homeworks-Labs-Exercises/04Functions-Lab/06Sign-Check.js
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/04Functions-Lab/06Sign-Check.js
@@ -1,4 +1,22 @@
 function signCheck(numOne, numTwo, numThree) {
+  let totalNegative = 0;
+  if (isNegative(numOne)) {
+    totalNegative++;
+  }
+  if (isNegative(numTwo)) {
+    totalNegative++;
+  }
+  if (isNegative(numThree)) {
+    totalNegative++;
+  }
 
+  if (totalNegative % 2 === 0) {
+    console.log("Positive");
+  } else {
+    console.log("Negative");
+  }
+  function isNegative(number) {
+    return number < 0;
+  }
 }
-signCheck(5, 12, -15)
+signCheck(-6, -12, 14);

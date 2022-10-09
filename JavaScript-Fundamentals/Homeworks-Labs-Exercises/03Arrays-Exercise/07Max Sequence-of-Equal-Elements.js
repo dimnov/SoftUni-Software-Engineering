b89c5fb5b0ec658ec95cf6d19452dc284e5055a5
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-09 22:49:22 +0300

    Update 07Max Sequence-of-Equal-Elements.js

--- JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Exercise/07Max Sequence-of-Equal-Elements.js
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Exercise/07Max Sequence-of-Equal-Elements.js
@@ -1,4 +1,36 @@
-function maxSequenceOfEqualElements(arr) {
-  //todo
+function maxSequence(arr) {
+  let longestSequence = [];
+  let leftMost = 0;
+
+  let arrLength = arr.length;
+
+  for (let i = 0; i < arrLength; i++) {
+    let current = Number(arr[i]);
+    let currentSequence = [current];
+
+
+    for (let j = i + 1; j < arrLength; j++) {
+      let nextEl = Number(arr[j]);
+
+      if (nextEl == current) {
+        currentSequence.push(nextEl);
+      } else {
+        break;
+      }
+    }
+
+    if (currentSequence.length > longestSequence.length) {
+      longestSequence = [];
+
+      for (let j = 0; j < currentSequence.length; j++) {
+        longestSequence.push(currentSequence[j]);
+      }
+    } else {
+      if (i < leftMost) {
+        leftMost = i;
+      }
+    }
+  }
+  console.log(longestSequence.join(' '));
 }
-maxSequenceOfEqualElements([2, 1, 1, 2, 3, 3, 2, 2, 2, 1])
+maxSequence([1, 1, 1, 2, 3, 1, 3, 3]);

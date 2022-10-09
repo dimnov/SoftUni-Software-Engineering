0ab957dd3754b8a897941f0d9d8d0ef59eb4195f
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-09 22:49:26 +0300

    Update 08Magic-Sum.js

--- JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Exercise/08Magic-Sum.js
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/03Arrays-Exercise/08Magic-Sum.js
@@ -1,4 +1,12 @@
 function magicSum(arr, magicNum) {
-  //todo
+  let arrLength = arr.length;
+
+  for (let i = 0; i <= arrLength; i++) {
+    for (let j = i + 1; j < arrLength; j++) {
+      if (Number(arr[i]) + Number(arr[j]) == magicNum) {
+        console.log(`${arr[i]} ${arr[j]}`);
+      }
+    }
+  }
 }
-magicSum([1, 7, 6, 2, 19, 23], 8)
+magicSum([14, 20, 60, 13, 7, 19, 8], 27)

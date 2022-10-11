3cdce1905b903f2c78136c9e1ba552d0b3ac802c
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-11 22:40:27 +0300

    Update 05Tseam-Account.js

--- JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/05Tseam-Account.js
+++ JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/05Tseam-Account.js
@@ -1,4 +1,35 @@
-function tseamAccount() {
+function tseamAccount(arr) {
+  let games = arr[0].split(' ');
+  let arrLength = arr.length;
+  let index;
 
+  for (let i = 1; i < arrLength; i++) {
+    let current = arr[i].split(' ');
+    let command = current[0];
+    let gameName = current[1];
+
+    if (command === 'Play') {
+      break;
+    } else if (command === 'Install' && games.includes(gameName) === false) {
+      games.push(gameName);
+    } else if (command === 'Uninstall' && games.includes(gameName)) {
+      index = games.indexOf(gameName);
+      games.splice(index, 1);
+    } else if (command === 'Update' && games.includes(gameName)) {
+      index = games.indexOf(gameName);
+      games.splice(index, 1);
+      games.push(gameName);
+    } else if (command === 'Expansion') {
+      let expansion = gameName.split('-');
+      let game = expansion[0];
+
+      if (games.includes(game)) {
+        let expandedGame = expansion.join(':');
+        index = games.indexOf(game);
+        games.splice(index + 1, 0, expandedGame);
+      }
+    }
+  }
+  console.log(games.join(' '));
 }
-tseamAccount(['CS WoW Diablo', 'Install LoL', 'Uninstall WoW', 'Update Diablo', 'Expansion CS-Go', 'Play!'])
+tseamAccount(['CS WoW Diablo', 'Uninstall XCOM', 'Update PeshoGame', 'Update WoW', 'Expansion Civ-V', 'Play!'])

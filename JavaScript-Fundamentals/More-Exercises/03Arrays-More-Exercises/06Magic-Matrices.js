5cfb756bc85da81f6a04ada463f0286a20ab5084
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-11 22:40:30 +0300

    Update 06Magic-Matrices.js

--- JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/06Magic-Matrices.js
+++ JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/06Magic-Matrices.js
@@ -1,4 +1,21 @@
-function magicMatrices() {
+function magicMatrices(arr) {
+  let sum = arr[0].reduce((sum, x) => sum + x);
 
+  for (let rows = 0; rows < arr.length; rows++) {
+    let rowsSum = 0;
+    let columnsSum = 0;
+
+    for (let columns = 0; columns < arr[rows].length; columns++) {
+      if (columns < arr.length) {
+        rowsSum += arr[columns][rows];
+      }
+      columnsSum += arr[rows][columns];
+    }
+
+    if (rowsSum != sum || columnsSum != sum) {
+      return false;
+    }
+  }
+  return true;
 }
-magicMatrices([[4, 5, 6], [6, 5, 4], [5, 5, 5]])
+console.log(magicMatrices([[4, 5, 6], [6, 5, 4], [5, 5, 5]]));

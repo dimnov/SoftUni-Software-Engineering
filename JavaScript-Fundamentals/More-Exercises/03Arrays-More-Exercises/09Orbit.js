5d498c4bd109dc2d4afc4302c90be94493ef8c75
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-14 16:53:55 +0300

    Update 09Orbit.js

--- JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/09Orbit.js
+++ JavaScript-Fundamentals/More-Exercises/03Arrays-More-Exercises/09Orbit.js
@@ -1,4 +1,20 @@
-function orbit() {
+function orbit(arr) {
+  let width = arr[0];
+  let height = arr[1];
+  let x = arr[2];
+  let y = arr[3];
 
+  let matrix = [];
+
+  for (let i = 0; i < width; i++) {
+    matrix.push([]);
+  }
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      matrix[i][j] = Math.max(Math.abs(i - x), Math.abs(j - y)) + 1;
+    }
+  }
+  console.log(matrix.map(row => row.join(' ')).join('\n'));
 }
-orbit([4, 4, 0, 0])
+orbit([5, 5, 2, 2]);

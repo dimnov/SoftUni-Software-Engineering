c8482bd3117335cbfc809452a62a348418264488
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-15 21:25:35 +0300

    Update 06Bomb-Numbers.js

--- JavaScript-Fundamentals/Homeworks-Labs-Exercises/05Arrays-Advanced-Exercise/06Bomb-Numbers.js
+++ JavaScript-Fundamentals/Homeworks-Labs-Exercises/05Arrays-Advanced-Exercise/06Bomb-Numbers.js
@@ -1,6 +1,25 @@
-function bombNumbers() {
+function bombNumbers(arr, specialNum) {
+  let sum = 0;
+  let arrLength = arr.length;
 
+  for (let i = 0; i < arrLength; i++) {
+    let current = arr[i];
+
+    if (current === specialNum[0]) {
+      let start = Math.max(0, i - specialNum[1]);
+      let countToEnd = (specialNum[1] * 2) + 1;
+
+      arr.splice(start, countToEnd);
+      i = i - specialNum[1] - 1;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    sum += current;
+  }
+  console.log(sum);
 }
 bombNumbers([1, 2, 2, 4, 2, 2, 2, 9],
   [4, 2]
-)
+);

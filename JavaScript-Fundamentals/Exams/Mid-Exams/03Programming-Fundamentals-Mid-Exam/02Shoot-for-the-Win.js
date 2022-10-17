c5e7a645ca0f27b3d5a691b69b032379a0cd9d1f
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-17 22:57:37 +0300

    Update 02Shoot-for-the-Win.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/03Programming-Fundamentals-Mid-Exam/02Shoot-for-the-Win.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/03Programming-Fundamentals-Mid-Exam/02Shoot-for-the-Win.js
@@ -1,15 +1,37 @@
-function shootForTheWin() {
+function shootForTheWin(arr) {
 
+  let targets = arr.shift().split(' ').map(Number);
+  let index = arr.shift();
+  const targetsLength = targets.length;
 
+  let total = 0;
 
+  while (index !== 'End') {
+    index = Number(index);
 
+    if (index < targetsLength) {
 
+      for (let i = 0; i < targetsLength; i++) {
+        let current = targets[index];
 
-
+        if (i !== index && targets[i] !== -1) {
+          if (targets[i] > current) {
+            targets[i] -= current;
+          } else {
+            targets[i] += current;
+          }
+        }
+      }
+      targets[index] = -1;
+      total++;
+    }
+    index = arr.shift();
+  }
+  console.log(`Shot targets: ${total} -> ${targets.join(' ')}`);
 }
-shootForTheWin((["24 50 36 70",
-  "0",
+shootForTheWin((["30 30 12 60 54 66",
+  "5",
+  "2",
   "4",
-  "3",
-  "1",
+  "0",
   "End"]));

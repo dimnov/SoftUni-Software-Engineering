f455f1c4a7fb1925e167f743ce28059f7b7898e0
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-20 22:47:50 +0300

    Update 03Heart-Delivery.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/04Programming-Fundamentals-Mid-Exam/03Heart-Delivery.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/04Programming-Fundamentals-Mid-Exam/03Heart-Delivery.js
@@ -1,4 +1,16 @@
-function heartDelivery() {
+function heartDelivery(arr) {
+  let products = arr.shift().split('@');
+  let current = arr.shift();
 
+  while (current !== 'Love!') {
+    let command = current.split(' ');
+
+
+
+    current = arr.shift();
+  }
 }
-heartDelivery()
+heartDelivery(["10@10@10@2",
+  "Jump 1",
+  "Jump 2",
+  "Love!"]);

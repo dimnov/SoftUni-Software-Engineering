c85833c43ae8166932afde527da9cf37369c6947
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-22 21:48:28 +0300

    Update 01Black-Flag.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/06Programming-Fundamentals-Mid-Exam/01Black-Flag.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/06Programming-Fundamentals-Mid-Exam/01Black-Flag.js
@@ -1,4 +1,30 @@
-function blackFlag() {
+function blackFlag(arr) {
+  let days = Number(arr.shift());
+  let dailyPlunder = Number(arr.shift());
+  let expectedPlunder = Number(arr.shift());
 
+  let sum = 0;
+  let day = 0;
+
+  for (let i = 0; i < days; i++) {
+    sum += dailyPlunder;
+    day++;
+
+    if (day % 3 === 0) {
+      sum += dailyPlunder / 2;
+    }
+
+    if (day % 5 === 0) {
+      sum *= 0.7;
+    }
+  }
+  if (sum >= expectedPlunder) {
+    console.log(`Ahoy! ${sum.toFixed(2)} plunder gained.`);
+  } else {
+    let percentage = (sum / expectedPlunder) * 100;
+    console.log(`Collected only ${percentage.toFixed(2)}% of the plunder.`);
+  }
 }
-blackFlag();
+blackFlag(["5",
+  "40",
+  "100"]);

9a5d6acb29a1e3e424e443b02c4f379f2e1ab000
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-22 21:48:22 +0300

    Update 01Bonus-Scoring-System.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/05Programming-Fundamentals-Mid-Exam/01Bonus-Scoring-System.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/05Programming-Fundamentals-Mid-Exam/01Bonus-Scoring-System.js
@@ -1,4 +1,25 @@
-function bonusScoring() {
+function bonusScoring(arr) {
 
+  let students = Number(arr.shift());
+  let lectures = Number(arr.shift());
+  let bonus = Number(arr.shift());
+
+  let maxStudent = 0;
+  let maxBonus = 0;
+
+  for (let i = 0; i < students; i++) {
+    let student = Number(arr[i]) / lectures * (5 + bonus);
+
+    if (student >= maxBonus) {
+      maxBonus = student;
+      maxStudent = Number(arr[i]);
+    }
+  }
+
+  console.log(`Max Bonus: ${Math.ceil(maxBonus)}.`);
+  console.log(`The student has attended ${maxStudent} lectures.`);
 }
-bonusScoring();
+bonusScoring([
+  '5', '25', '30',
+  '12', '19', '24',
+  '16', '20']);

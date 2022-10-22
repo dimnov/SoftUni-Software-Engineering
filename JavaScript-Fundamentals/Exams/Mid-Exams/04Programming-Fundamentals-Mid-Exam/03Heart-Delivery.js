9d72e02e34f3543b19cf62487f9f99219599dbd8
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-22 21:48:20 +0300

    Update 03Heart-Delivery.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/04Programming-Fundamentals-Mid-Exam/03Heart-Delivery.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/04Programming-Fundamentals-Mid-Exam/03Heart-Delivery.js
@@ -1,16 +1,53 @@
 function heartDelivery(arr) {
-  let products = arr.shift().split('@');
+  let numbers = arr.shift().split('@').map(Number);
   let current = arr.shift();
+
+  let currentIndex = 0;
 
   while (current !== 'Love!') {
     let command = current.split(' ');
+    let jumps = Math.abs(command[1]);
 
+    currentIndex += jumps;
 
+    if (currentIndex >= numbers.length) {
+      currentIndex = 0;
+    }
 
+    if (numbers[currentIndex] === 0) {
+      console.log(`Place ${currentIndex} already had Valentine's day.`);
+    } else {
+      numbers[currentIndex] -= 2;
+
+      if (numbers[currentIndex] === 0) {
+        console.log(`Place ${currentIndex} has Valentine's day.`);
+      }
+    }
     current = arr.shift();
   }
+
+  let isTrue = true;
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== 0) {
+      isTrue = false;
+      sum++;
+    }
+  }
+
+  console.log(`Cupid's last position was ${currentIndex}.`);
+
+  if (isTrue) {
+    console.log(`Mission was successful.`);
+  } else {
+    console.log(`Cupid has failed ${sum} places.`);
+  }
 }
-heartDelivery(["10@10@10@2",
-  "Jump 1",
+heartDelivery(["2@4@2",
   "Jump 2",
+  "Jump 2",
+  "Jump 8",
+  "Jump 3",
+  "Jump 1",
   "Love!"]);

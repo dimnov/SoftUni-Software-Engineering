8d8bbe86297379176049106490c99b8d2fd06a26
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-22 21:48:26 +0300

    Update 03Inventory.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/05Programming-Fundamentals-Mid-Exam/03Inventory.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/05Programming-Fundamentals-Mid-Exam/03Inventory.js
@@ -1,4 +1,59 @@
-function inventory() {
+function inventory(arr) {
+  let inventory = arr.shift().split(', ');
+  let commands = arr.shift();
 
+  while (commands !== 'Craft!') {
+    let current = commands.split(' - ');
+    let command = current[0];
+    let item = current[1];
+
+    switch (command) {
+      case 'Collect':
+        if (inventory.includes(item)) {
+          break;
+        } else {
+          inventory.push(item);
+        }
+        break;
+
+      case 'Drop':
+        if (inventory.includes(item)) {
+          let index = inventory.indexOf(item);
+
+          if (index > -1) {
+            inventory.splice(index, 1);
+          }
+        } else {
+          break;
+        }
+        break;
+
+      case 'Combine Items':
+        let splitItems = current[1].split(':');
+        let index = inventory.indexOf(splitItems[0]);
+
+        if (index >= 0) {
+          inventory.splice(index + 1, 0, splitItems[1]);
+        }
+        break;
+
+      case 'Renew':
+
+        let i = inventory.indexOf(item);
+        if (i >= 0) {
+          inventory.splice(i, 1);
+          inventory.push(item);
+        }
+        break;
+    }
+    commands = arr.shift();
+  }
+  console.log(inventory.join(', '));
 }
-inventory();
+inventory([
+  'Iron, Sword',
+  'Drop - Bronze',
+  'Combine Items - Sword:Bow',
+  'Combine Items - v:Bow',
+  'Renew - Iron',
+  'Craft!']);

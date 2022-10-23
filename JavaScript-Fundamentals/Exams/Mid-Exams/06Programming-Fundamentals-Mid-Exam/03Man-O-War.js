ab3180e970be393a1dc5b6b788766e349345edda
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-23 12:17:33 +0300

    Update 03Man-O-War.js

--- JavaScript-Fundamentals/Exams/Mid-Exams/06Programming-Fundamentals-Mid-Exam/03Man-O-War.js
+++ JavaScript-Fundamentals/Exams/Mid-Exams/06Programming-Fundamentals-Mid-Exam/03Man-O-War.js
@@ -1,33 +1,76 @@
 function manOWar(arr) {
-  let pirateShip = arr.shift().split('>');
-  let warShip = arr.shift().split('>');
+  let pirateShip = arr.shift().split('>').map(Number);
+  let warShip = arr.shift().split('>').map(Number);
   let maxHealth = Number(arr.shift());
   let line = arr.shift();
+
+  let death = false;
 
   while (line !== 'Retire') {
     let current = line.split(' ');
     let command = current[0];
-    let numOne = current[1];
-    let numTwo = current[2];
-    let numThree = current[3];
+    let numOne = Number(current[1]);
+    let numTwo = Number(current[2]);
+    let numThree = Number(current[3]);
 
     switch (command) {
       case 'Fire':
-        if (pirateShip.includes())
-          break;
+        if (numOne >= 0 && numOne <= warShip.length - 1) {
+          warShip[numOne] -= numTwo;
+
+          if (warShip[numOne] <= 0) {
+            console.log("You won! The enemy ship has sunken.");
+            return;
+          }
+        }
+
+        line = arr.shift();
+        break;
+
       case 'Defend':
+        if (numOne >= 0 && numOne <= pirateShip.length - 1 && numTwo >= 0 && numTwo <= pirateShip.length - 1) {
+          for (let i = numOne; i <= numTwo; i++) {
+            pirateShip[i] -= numThree;
 
+            if (pirateShip[i] <= 0) {
+              console.log("You lost! The pirate ship has sunken.");
+              return;
+            }
+          }
+        }
+
+        line = arr.shift();
         break;
+
       case 'Repair':
+        if (numOne >= 0 && numOne <= pirateShip.length - 1) {
+          pirateShip[numOne] += numTwo;
+          if (pirateShip[numOne] > maxHealth) {
+            pirateShip[numOne] = maxHealth;
+          }
+        }
 
+        line = arr.shift();
         break;
-      case 'Status':
 
+      case 'Status':
+        let counter = 0;
+        for (let i = 0; i < pirateShip.length; i++) {
+          if (pirateShip[i] < maxHealth * 0.2) {
+            counter++;
+          }
+        }
+        console.log(`${counter} sections need repair.`);
+
+        line = arr.shift();
         break;
     }
-    console.log(command);
-    line = arr.shift();
   }
+  let sumPirateShip = pirateShip.reduce((a, b) => a + b, 0);
+  let sumWarShip = warShip.reduce((a, b) => a + b, 0);
+
+  console.log(`Pirate ship status: ${sumPirateShip}`);
+  console.log(`Warship status: ${sumWarShip}`)
 }
 manOWar(["12>13>11>20>66",
   "12>22>33>44>55>32>18",

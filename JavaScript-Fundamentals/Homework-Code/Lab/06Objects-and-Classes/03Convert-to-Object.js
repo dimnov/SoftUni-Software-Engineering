3c221097e0ef10ce5db0e1929d1be9441fbba7e1
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-27 22:16:33 +0300

    Update 03Convert-to-Object.js

--- JavaScript-Fundamentals/Homework-Code/Lab/06Objects-and-Classes/03Convert-to-Object.js
+++ JavaScript-Fundamentals/Homework-Code/Lab/06Objects-and-Classes/03Convert-to-Object.js
@@ -1,4 +1,8 @@
-function convertObject() {
+function convertObject(jsonStr) {
+  let person = JSON.parse(jsonStr);
 
+  for (let key of Object.keys(person)) {
+    console.log(`${key}: ${person[key]}`);
+  }
 }
 convertObject('{"name": "George", "age": 40, "town": "Sofia"}');

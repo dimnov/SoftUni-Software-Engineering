227965a7b9b3b2a025c7c013146f18f05ec6759e
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-10-28 11:30:22 +0300

    Create trekkingMania.js

---
+++ JavaScript-Programming-Basics/Exams/07Exam-28-and-29-March-2020/trekkingMania.js
@@ -0,0 +1,55 @@
+function trekkingMania(input) {
+  let index = 0;
+
+  let group = Number(input[index]);
+  index++;
+
+  let musala = 0;
+  let montBlanc = 0;
+  let kilimanjaro = 0;
+  let k2 = 0;
+  let everest = 0;
+
+  let people = 0;
+
+  for (let i = 0; i < group; i++) {
+    let number = Number(input[index]);
+    people += number;
+    index++;
+    if (number <= 5) {
+      musala += number;
+    } else if (number >= 6 && number <= 12) {
+      montBlanc += number;
+    } else if (number >= 13 && number <= 25) {
+      kilimanjaro += number;
+    } else if (number >= 26 && number <= 40) {
+      k2 += number;
+    } else {
+      everest += number;
+    }
+
+  }
+
+  let percentMusala = (musala / people) * 100;
+  let percentmontBlanc = (montBlanc / people) * 100;
+  let percentkilimanjaro = (kilimanjaro / people) * 100;
+  let percentk2 = (k2 / people) * 100;
+  let percenteverest = (everest / people) * 100;
+
+  console.log(`${percentMusala.toFixed(2)}%`);
+  console.log(`${percentmontBlanc.toFixed(2)}%`);
+  console.log(`${percentkilimanjaro.toFixed(2)}%`);
+  console.log(`${percentk2.toFixed(2)}%`);
+  console.log(`${percenteverest.toFixed(2)}%`);
+}
+trekkingMania(["10",
+  "10",
+  "5",
+  "1",
+  "100",
+  "12",
+  "26",
+  "17",
+  "37",
+  "40",
+  "78"])

94a0db555697cbb5ce918dccb7e3b2aa6fa66835
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-11-18 23:51:54 +0200

    Update 05Card-Game.js

--- JavaScript-Fundamentals/Homework-Code/Exercise/07Associative-Arrays/05Card-Game.js
+++ JavaScript-Fundamentals/Homework-Code/Exercise/07Associative-Arrays/05Card-Game.js
@@ -1,5 +1,55 @@
 function cardGame(input) {
+  let players = new Map();
 
+  let cardPoints = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14,
+  };
+
+  let cardTypePoints = {
+    "S": 4,
+    "H": 3,
+    "D": 2,
+    "C": 1,
+  }
+
+  for (const line of input) {
+    let playerData = line.split(': ');
+    let name = playerData.shift();
+    let cards = playerData[0].split(', ');
+
+    if (!players.has(name)) {
+      players.set(name, new Set());
+    }
+
+    for (let card of cards) {
+      players.get(name).add(card);
+    }
+  }
+
+  for (let [key, value] of players) {
+    let sum = 0;
+    for (const card of value) {
+      let cardInfo = card.split('');
+      let cardType = cardInfo.pop();
+      let cardPower = cardInfo.join('');
+      let cardPowerAsNum = cardPoints[cardPower];
+      let cardTypeAsNum = cardTypePoints[cardType];
+      sum += cardPowerAsNum * cardTypeAsNum;
+    }
+    console.log(`${key}: ${sum}`);
+  }
 }
 cardGame([
   'Peter: 2C, 4H, 9H, AS, QS',

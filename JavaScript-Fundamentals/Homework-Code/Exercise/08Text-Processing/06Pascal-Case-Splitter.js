0270648ade837453bb85ec57be26cb7cfb8b7f4a
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-11-19 20:22:04 +0200

    Create 06Pascal-Case-Splitter.js

---
+++ JavaScript-Fundamentals/Homework-Code/Exercise/08Text-Processing/06Pascal-Case-Splitter.js
@@ -0,0 +1,14 @@
+function pascalCase(text) {
+  let result = text[0];
+  let lower = text.toLowerCase();
+
+  for (let i = 1; i < text.length; i++) {
+    if (text[i] !== lower[i]) {
+      result += ', ';
+    }
+    result += text[i];
+  }
+
+  console.log(result);
+}
+pascalCase('SplitMeIfYouCanHaHaYouCantOrYouCan');

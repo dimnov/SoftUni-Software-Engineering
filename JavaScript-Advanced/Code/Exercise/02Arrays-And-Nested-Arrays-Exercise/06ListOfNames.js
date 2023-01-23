fdba9951be33cf7c4da66790640cff7365e9ff93
dimnov <dimnov1512@gmail.com>
2023-01-23 17:45:22 +0200

    Create 06ListOfNames.js

---
+++ JavaScript-Advanced/Code/Exercise/02Arrays-And-Nested-Arrays-Exercise/06ListOfNames.js
@@ -0,0 +1,8 @@
+function listOfNames(arr) {
+  let result = arr.sort((a, b) => a.localeCompare(b));
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${i + 1}.${arr[i]}`);
+  }
+}
+listOfNames(["John", "Bob", "Christina", "Ema"]);

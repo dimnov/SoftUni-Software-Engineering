6cb5b32cbcd4d52e079528b9a0d0f8e8f10fe648
dimnov <dimnov1512@gmail.com>
2023-01-23 17:45:30 +0200

    Create 01City-Record.js

---
+++ JavaScript-Advanced/Code/Lab/03Objects-And-Composition-Lab/01City-Record.js
@@ -0,0 +1,11 @@
+function cityRecord(cityName, population, treasury) {
+  let city = {
+    name: cityName,
+    population: population,
+    treasury: treasury,
+  }
+  return city;
+}
+cityRecord('Tortuga',
+  7000,
+  15000);

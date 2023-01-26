0c77911915740b34c08ebd8de102dfd64b4ef7a9
dimnov <dimnov1512@gmail.com>
2023-01-26 13:17:45 +0200

    Update 06StoreCatalogue.js

--- JavaScript-Advanced/Code/Exercise/03Objects-And-Composition-Exercise/06StoreCatalogue.js
+++ JavaScript-Advanced/Code/Exercise/03Objects-And-Composition-Exercise/06StoreCatalogue.js
@@ -1,5 +1,26 @@
 function storeCatalogue(arr) {
+  const catalogue = {};
 
+  for (const line of arr) {
+    const [productName, price] = line.split(' : ');
+    const firstLetter = productName[0];
+
+    if (!catalogue.hasOwnProperty(firstLetter)) {
+      catalogue[firstLetter] = {};
+    }
+    catalogue[firstLetter][productName] = price;
+  }
+
+  const sorted = Object.keys(catalogue).sort((a, b) => a.localeCompare(b));
+
+  for (const letter of sorted) {
+    console.log(letter);
+
+    const sortedProducts = Object.keys(catalogue[letter]).sort((a, b) => a.localeCompare(b));
+    for (const product of sortedProducts) {
+      console.log(`  ${product}: ${catalogue[letter][product]}`);
+    }
+  }
 }
 storeCatalogue(['Appricot : 20.4',
   'Fridge : 1500',

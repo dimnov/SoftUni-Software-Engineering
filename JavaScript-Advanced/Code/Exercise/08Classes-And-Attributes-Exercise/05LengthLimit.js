aee25db450e6a14839f8ce8108b939a4be9b1767
dimnov <dimnov1512@gmail.com>
2023-02-14 12:27:41 +0200

    Create 05LengthLimit.js

---
+++ JavaScript-Advanced/Code/Exercise/08Classes-And-Attributes-Exercise/05LengthLimit.js
@@ -0,0 +1,65 @@
+// class Stringer {
+//   constructor(string, length) {
+//     this.innerString = string;
+//     this.innerLength = length;
+//   }
+
+//   increase(num) {
+//     this.innerLength += num;
+//   }
+
+//   decrease(num) {
+//     if (this.innerLength - num < 0) {
+//       this.innerLength = 0;
+//     } else {
+//       this.innerLength -= num;
+//     }
+//   }
+
+//   toString() {
+//     if (this.innerString.length > this.innerLength) {
+//       return `${this.innerString.substring(0, this.innerLength)}...`;
+//     } else {
+//       return this.innerString;
+//     }
+//   }
+// }
+
+
+class Stringer {
+  constructor(string, length) {
+    this.innerString = string;
+    this.innerLength = length;
+  }
+
+  increase(length) {
+    this.innerLength += length;
+  }
+
+  decrease(length) {
+    if (this.innerLength - length < 0) {
+      this.innerLength = 0;
+    } else {
+      this.innerLength -= length;
+    }
+  }
+
+  toString() {
+    if (this.innerString.length > this.innerLength) {
+      return `${this.innerString.substring(0, this.innerLength)}...`;
+    }
+    return this.innerString;
+  }
+}
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+
+test.decrease(3);
+console.log(test.toString()); // Te...
+
+test.decrease(5);
+console.log(test.toString()); // ...
+
+test.increase(4);
+console.log(test.toString()); // Test
